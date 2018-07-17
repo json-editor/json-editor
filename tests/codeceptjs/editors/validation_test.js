f0c8515454c6e8da1f15b6ba737f9d6f9fcc1cf5
Maarten Brouwers <github.com@murb.nl>
2018-07-17 20:43:54 +0200

    relative filepaths + extended testsuite * added support for relative file paths * added validation.html to test suite (implicitly tests relative file paths)

---
+++ tests/codeceptjs/editors/validation_test.js
@@ -0,0 +1,12 @@
+// Note: validation.html has its own way of testing; this test simply tests if the output of that test conforms to the expected output
+
+Feature('Validations');
+
+Scenario('test validations in validation.html', (I) => {
+    I.amOnPage('validation.html');
+    var numberOfTestItemsExpected = 129;
+    I.waitForElement("#output div:nth-child("+numberOfTestItemsExpected+")", 10);
+    I.seeNumberOfElements("#output div", numberOfTestItemsExpected);
+    I.see("success");
+    I.dontSee('Fail');
+});
