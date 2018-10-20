e141dedcf02c07fe744ccd9baf7296cdea11422e
Ruud <ruud@habing.net>
2018-10-20 12:17:26 +0200

    Added original Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -217,7 +217,7 @@ module.exports = function(grunt) {
 
     // Default task.
   grunt.registerTask('default', ['jshint:beforeconcat','concat','jshint:afterconcat','uglify','cssmin']);
-
+  
   grunt.registerTask('rawbuild', ['concat','uglify','cssmin']);
 
 };
