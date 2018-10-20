0bb60ef43990a934d8312ac6f6f21a44b75f9eb6
Ruud <ruud12@users.noreply.github.com>
2018-10-20 12:20:43 +0200

    Changed defaults.js to original

--- src/defaults.js
+++ src/defaults.js
@@ -13,9 +13,9 @@ JSONEditor.defaults.options.prompt_before_delete = true;
 JSONEditor.defaults.translate = function(key, variables) {
   var lang = JSONEditor.defaults.languages[JSONEditor.defaults.language];
   if(!lang) throw "Unknown language "+JSONEditor.defaults.language;
-
+  
   var string = lang[key] || JSONEditor.defaults.languages[JSONEditor.defaults.default_language][key];
-
+  
   if(typeof string === "undefined") throw "Unknown translate string "+key;
 
   if(variables) {
@@ -23,7 +23,7 @@ JSONEditor.defaults.translate = function(key, variables) {
       string = string.replace(new RegExp('\\{\\{'+i+'}}','g'),variables[i]);
     }
   }
-
+  
   return string;
 };
 
@@ -246,7 +246,7 @@ JSONEditor.plugins = {
 
   },
   select2: {
-
+  
   },
   selectize: {
   }
