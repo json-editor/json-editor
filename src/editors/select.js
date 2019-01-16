158baf33f31ce55844ff341c72882ca878ca2b63
Peter Klein <pmk@io.dk>
2019-01-16 16:21:55 +0100

    added typecasting on return value

--- src/editors/select.js
+++ src/editors/select.js
@@ -199,10 +199,10 @@ JSONEditor.defaults.editors.select = JSONEditor.AbstractEditor.extend({
     var new_val;
     // Invalid option, use first option instead
     if(this.enum_values.indexOf(String(val)) === -1) {
-      new_val = this.enum_values[0];
+      new_val =  this.typecast(this.enum_values[0]);
     }
     else {
-      new_val = this.enum_values[this.enum_values.indexOf(String(val))];
+      new_val =  this.typecast(this.enum_values[this.enum_values.indexOf(String(val))]);
     }
 
     // If valid hasn't changed
