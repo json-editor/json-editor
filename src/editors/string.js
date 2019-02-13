a6e69d1e51b0fa595574048da6f78c82263c1559
KU <ku@digitaldolphins.jp>
2019-02-13 18:19:35 +0900

    Add sqlserver editor format.

--- src/editors/string.js
+++ src/editors/string.js
@@ -148,6 +148,7 @@ JSONEditor.defaults.editors.string = JSONEditor.AbstractEditor.extend({
           'scss',
           'smarty',
           'sql',
+          'sqlserver',
           'stylus',
           'svg',
           'twig',
