8aa650093a2075a245acc9207b8905b0358b3608
Peter Klein <pmk@io.dk>
2019-05-29 23:24:57 +0200

    DescribedBy Editor

--- src/defaults.js
+++ src/defaults.js
@@ -367,3 +367,14 @@ JSONEditor.defaults.resolvers.unshift(function(schema) {
 JSONEditor.defaults.resolvers.unshift(function(schema) {
   if (schema.type === "string" && schema.format === "starrating") return "starrating";
 });
+
+// hyper-link describeBy Resolver
+JSONEditor.defaults.resolvers.unshift(function(schema) {
+  if (schema.links) {
+    for (var i = 0; i < schema.links.length; i++) {
+      if (schema.links[i].rel.toLowerCase() === "describedby") {
+        return "describedBy";
+      }
+    }
+  }
+});
