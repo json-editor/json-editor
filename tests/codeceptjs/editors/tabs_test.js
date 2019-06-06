e791a2b7129089adca5dd1b5e5e3f300018a1ce6
germanbisurgi <gebisurgi@gmail.com>
2019-06-06 16:05:03 +0200

    Run multiple also in nam script. Refactored tests (no global vars). Fix for tabs test.

--- tests/codeceptjs/editors/tabs_test.js
+++ tests/codeceptjs/editors/tabs_test.js
@@ -1,13 +1,12 @@
-
 Feature('Tabs');
 
 Scenario('test top-tabs', (I) => {
     I.amOnPage('tabs.html');
-    I.click('Add Person');
+    I.waitForElement('[data-schemapath="root"] .json-editor-btn-add');
+    I.click('[data-schemapath="root"] .json-editor-btn-add');
     I.see('1 -');
     I.see('validation failed')
     I.fillField('root[1][name]', 'Somebody')
-    //pause();
     I.click('body')
     I.see('validation passed')
 });
