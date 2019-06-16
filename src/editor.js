be41d40d4a92909ce351289ad9f7b707a68d005d
Peter Klein <pmk@io.dk>
2019-06-16 14:02:15 +0200

    Fixed problems with dependencies not working with boolean format

--- src/editor.js
+++ src/editor.js
@@ -94,7 +94,7 @@ JSONEditor.AbstractEditor = Class.extend({
   },
   checkDependency: function(path, choices) {
     var wrapper = this.container || this.control;
-    if (this.path === path || !wrapper || this.jsoneditor === null) {   
+    if (this.path === path || !wrapper || this.jsoneditor === null) {
       return;
     }
 
@@ -132,7 +132,7 @@ JSONEditor.AbstractEditor = Class.extend({
       this.dependenciesFulfilled = value === choices;
     } else if (typeof choices === 'boolean') {
       if (choices) {
-        this.dependenciesFulfilled = value && value.length > 0;
+        this.dependenciesFulfilled = value || value.length > 0;
       } else {
         this.dependenciesFulfilled = !value || value.length === 0;
       }
