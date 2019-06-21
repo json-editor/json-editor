86223292c8df0311a0ff8e71c1940c3d3cf59a4a
germanbisurgi <gebisurgi@gmail.com>
2019-06-21 16:55:44 +0200

    wait for ready

--- tests/codeceptjs/editors/checkbox_test.js
+++ tests/codeceptjs/editors/checkbox_test.js
@@ -4,5 +4,6 @@ Feature('checkbox');
 
 Scenario('should be disabled if "readonly" is specified', async (I) => {
   I.amOnPage('read-only.html');
+  I.waitForText('READY', 5, '.state')
   assert.equal(await I.grabAttributeFrom('[name="root[checkbox]"]', 'disabled'), 'true');
 });
