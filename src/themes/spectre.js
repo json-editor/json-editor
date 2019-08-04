d26dafc5228cb53564b7614cc34eb127af3457e3
Peter Klein <pmk@io.dk>
2019-08-04 12:54:59 +0200

    Spectre theme v0.9

--- src/themes/spectre.js
+++ src/themes/spectre.js
@@ -1,3 +1,4 @@
+// Spectre Theme using Spectre CSS framework. <https://picturepan2.github.io/spectre/index.html>
 JSONEditor.defaults.themes.spectre = JSONEditor.AbstractTheme.extend({
   // Config options that allows changing various aspects of the output
   options: {
@@ -6,30 +7,34 @@ JSONEditor.defaults.themes.spectre = JSONEditor.AbstractTheme.extend({
     object_indent: false,         // Indent nested object elements
     object_border: false,         // Add border around object elements
     table_border: false,          // Add border to array "table" row and cells
-    table_hdiv: false,            // Add bottom-border to array "table" cells
     table_zebrastyle: false,      // Add "zebra style" to array "table" rows
-    input_size: 'normal'          // Size of input and select elements. "small", "normal", "large"
+    input_size: 'normal',         // Size of input and select elements. "small", "normal", "large"
+    button_align: 'center'        // Alignment of button editor buttons. "left", "center", rignt"
   },
   // Custom stylesheet rules. (Does not suppert comma separated selectors)
   //  Will create a stylesheet in document head with the id "theme-spectre" if not exists.
   rules: {
-    'textarea': 'width:100%;min-height: 2rem;resize:vertical',
-    'table': 'border-collapse: collapse;',
-    'div[data-schematype]:not([data-schematype="object"])' : 'transition:.5s',
-    'div[data-schematype]:not([data-schematype="object"]):hover' : 'background-color: #eee',
-    '.je-table-zebra:nth-child(even)': 'background-color: #f2f2f2;',
-    '.je-table-border': 'border: 1px solid black;',
-    '.je-table-hdiv': 'border-bottom: 1px solid black;',
-    '.btn-info' : 'font-size:.5rem;font-weight:bold;height:1rem;padding:.2rem 0;line-height:1;margin:.3rem 0;',
-    '.btn-action.btn-info': 'width: 1rem;',
-    '.je-border': 'border:.05rem solid #dadee4',
+    '.slider:focus': 'box-shadow: none', // Remove slider focus shadow
+    'h3>label+.btn-group': 'margin-left:1rem', // Add margin between header and top buttons
+    '.json-editor-btntype-properties+div': 'font-size: .8rem;font-weight: normal;', // Fix fontsize in Properties modal
+    'textarea': 'width:100%;min-height: 2rem;resize:vertical',  // Prevent textarea from being resized horizontally
+    'table': 'border-collapse: collapse;', // Remove gap between table element borders
+    '.table td': 'padding: .4rem .4rem;', // reduce table padding
+    '.mr-5': 'margin-right: 1rem !important;', // margin for checkbox label
+    'div[data-schematype]:not([data-schematype="object"])' : 'transition:.5s',  // Hover on input block
+    'div[data-schematype]:not([data-schematype="object"]):hover' : 'background-color: #eee', // Hover on input block
+    '.je-table-border td': 'border: .05rem solid #dadee4', // Option: table_border
+    '.btn-info' : 'font-size:.5rem;font-weight:bold;height:.8rem;padding:.15rem 0;line-height:.8;margin:.3rem 0 .3rem .1rem;', // Infobutton
+    '.je-label': 'font-weight: 600', // Option: label_bold
+    '.btn-action.btn-info': 'width: .8rem;', // Infobutton
+    '.je-border': 'border:.05rem solid #dadee4', // Option: object_border
     '.je-panel': 'padding:.2rem;margin:.2rem;background-color: rgba(218,222,228,.1)',
     '.je-panel-top': 'padding:.2rem;margin:.2rem;background-color: rgba(218,222,228,.1)',
-    '.required:after': 'content: " *";color: red;font:inherit',
-    '.je-desc': 'font-size: smaller;margin: .2rem 0;',
+    '.required:after': 'content: " *";color: red;font:inherit', // Red * after label if field is required
+    '.je-desc': 'font-size: smaller;margin: .2rem 0;', // Description
 /*    '.columns': 'border:1px solid rgba(255,0,0,.5);',
     '.columns .columns': 'border:1px solid rgba(0,255,0,.5);',*/
-    '.columns .container.je-noindent': 'padding-left:0;padding-right:0;'
+    '.columns .container.je-noindent': 'padding-left:0;padding-right:0;' // Option: object_indent
   },
   // Functions for setting up the grid container, row and columns
   setGridColumnSize: function(el,size, offset) {
@@ -71,31 +76,48 @@ JSONEditor.defaults.themes.spectre = JSONEditor.AbstractTheme.extend({
     return el;
   },
 
+  // Button functions
   getHeaderButtonHolder: function() {
     var el = this.getButtonHolder();
-    el.classList.add('btn-group', 'ml-3');
     return el;
   },
+  // Button holder for the buttons
   getButtonHolder: function() {
     var el = this._super();
-    el.classList.add('btn-group', 'ml-3');
+    el.classList.add('btn-group');
+    return el;
+  },
+  getFormButtonHolder: function() {
+    var el = this._super();
+    el.classList.remove('btn-group');
+    if (this.options.button_align === 'center') el.classList.add('text-center');
+    else if (this.options.button_align === 'right') el.classList.add('text-right');
+    return el;
+  },
+  getFormButton: function(text, icon, title) {
+    var el = this._super(text, icon, title);
+    el.classList.add('btn', 'btn-primary', 'mx-2', 'my-1');
+
+    if (this.options.input_size === 'small') el.classList.add('btn-sm');
+    else if (this.options.input_size === 'large') el.classList.add('btn-lg');
     return el;
   },
   getButton: function(text, icon, title) {
     var el = this._super(text, icon, title);
-    el.classList.add('btn', 'btn-primary', 'ml-2', 'my-1');
+    el.classList.add('btn', 'btn-primary', 'mr-2', 'my-1');
     return el;
   },
+
   getFormInputDescription: function(text) {
     var el = this._super(text);
-    el.classList.add('je-desc');
+    el.classList.add('je-desc','hide-sm');
     return el;
   },
 
   /* Label for all elements except checkbox and radio */
   getFormInputLabel: function(text, req) {
     var el = this._super(text, req);
-    if (this.options.label_bold) el.classList.add('text-bold');
+    if (this.options.label_bold) el.classList.add('je-label');
     return el;
   },
 
@@ -106,11 +128,21 @@ JSONEditor.defaults.themes.spectre = JSONEditor.AbstractTheme.extend({
     return el;
   },
   getCheckboxLabel: function(text, req) {
-    var el = this.getFormInputLabel(text, req);
+    var el = this._super(text, req), icon = document.createElement('i');
+    icon.classList.add('form-icon');
+    el.classList.add('form-checkbox', 'mr-5');
+    el.insertBefore(icon, el.firstChild);
     return el;
   },
-
-
+  getFormCheckboxControl: function(label, input, compact) {
+    label.insertBefore(input, label.firstChild); // Move input into label element
+    if (compact) label.classList.add('form-inline');
+    return label;
+  },
+  getMultiCheckboxHolder: function(controls,label,description, infoText) {
+console.log('mul');
+    return this._super(controls,label,description, infoText);
+  },
   // Radio elements
   getFormRadio: function(attributes) {
     var el = this.getFormInputField('radio');
@@ -132,27 +164,7 @@ JSONEditor.defaults.themes.spectre = JSONEditor.AbstractTheme.extend({
     return label;
   },
 
-  getRangeInput: function(min, max, step) {
-    // ToDo: Move creaton of "output" element from "string" editor into theme.
-    var el = this.getFormInputField('range');
-    el.classList.add('slider', 'tooltip');
-    el.classList.remove('form-input');
-    el.setAttribute('oninput', 'this.setAttribute("value", this.value)');
-    el.setAttribute('min', min);
-    el.setAttribute('max', max);
-    el.setAttribute('step', step);
-    return el;
-  },
-  getSelectInput: function(options) {
-    var el = this._super(options);
-    el.classList.add('form-select');
-    return el;
-  },
-  getTextareaInput: function() {
-    var el = document.createElement('textarea');
-    el.classList.add('form-input');
-    return el;
-  },
+  // Create input field
   getFormInputField: function(type) {
     var el = this._super(type);
     if (['checkbox', 'radio'].indexOf(type) < 0) {
@@ -160,56 +172,61 @@ JSONEditor.defaults.themes.spectre = JSONEditor.AbstractTheme.extend({
     }
     return el;
   },
-  getFormCheckboxControl: function(label, input, compact) {
-    //var el = document.createElement('div');
-    //el.classList.add('form-group');
-      var icon = document.createElement('i');
-      icon.classList.add('form-icon');
-      label.appendChild(icon);
-      label.classList.add('form-' + input.type);
-    //el.appendChild(label);
-    input.style.width = 'auto';
-    label.insertBefore(input, label.firstChild);
-    if (compact) label.classList.add('form-inline');
-
-    return label;
+  // Create input field for type="range"
+  getRangeInput: function(min, max, step) {
+    var el = this.getFormInputField('range');
+    el.classList.add('slider');
+    el.classList.remove('form-input');
+    el.setAttribute('oninput', 'this.setAttribute("value", this.value)');
+    el.setAttribute('min', min);
+    el.setAttribute('max', max);
+    el.setAttribute('step', step);
+    return el;
   },
+  getRangeControl: function(input, output) {
+    var el = this._super(input, output);
+    el.classList.add('text-center');
+    return el;
+  },
+
+
+  // Create select box field
+  getSelectInput: function(options) {
+    var el = this._super(options);
+    el.classList.add('form-select');
+    return el;
+  },
+  // Create textarea field
+  getTextareaInput: function() {
+    var el = document.createElement('textarea');
+    el.classList.add('form-input');
+    return el;
+  },
+
 
   getFormControl: function(label, input, description, infoText) {
     var group = document.createElement('div');
     group.classList.add('form-group');
 
-    if (['checkbox', 'radio'].indexOf(input.type) > -1) {
-      if (infoText && label) group.appendChild(infoText);
-
-      // In compact mode there is no label element present
-      if (!label) label = this.getCheckboxLabel('');
-
-      label.appendChild(input);
-      label.classList.add('form-' + input.type);
-      if (infoText) label.style.marginRight = '2rem';
-      var icon = document.createElement('i');
-      icon.classList.add('form-icon');
-      label.appendChild(icon);
-      group.appendChild(label);
-    }
-    else {
-      if (label) {
-        label.classList.add('form-label');
-        group.appendChild(label);
-        if (infoText) group.insertBefore(infoText, group.firstChild);
+    if (label) {
+      if (input.type === 'checkbox') {
+        label = this.getFormCheckboxControl(label, input, false);
       }
-
-      if (this.options.input_size === 'small') input.classList.add('input-sm', 'select-sm');
-      else if (this.options.input_size === 'large') input.classList.add('input-lg', 'select-lg');
-      group.appendChild(input);
+      label.classList.add('form-label');
+      group.appendChild(label);
+      if (infoText) group.insertBefore(infoText, group.firstChild);
     }
+
+    if (this.options.input_size === 'small') input.classList.add('input-sm', 'select-sm');
+    else if (this.options.input_size === 'large') input.classList.add('input-lg', 'select-lg');
+    if (input.type !== 'checkbox') group.appendChild(input);
 
     if (description) group.appendChild(description);
 
     return group;
   },
 
+  // Create input group (input field with buttons)
   getInputGroup: function(input, buttons) {
     if (!input) return;
 
@@ -224,14 +241,14 @@ JSONEditor.defaults.themes.spectre = JSONEditor.AbstractTheme.extend({
     return inputGroup;
   },
 
-  // Button for displaying infotext tooltip
+  // Create button for displaying infotext tooltip
   getInfoButton: function(text) {
 
     var popover = document.createElement('div');
-    popover.classList.add('popover','float-right');
+    popover.classList.add('popover', 'popover-left', 'float-right');
 
     var button = document.createElement('button');
-    button.classList.add('btn', 'btn-primary', 'btn-info', 'btn-action', 's-circle');
+    button.classList.add('btn', 'btn-secondary', 'btn-info', 'btn-action', 's-circle');
     button.setAttribute('tabindex', '-1'); // exclude element from tab order
     popover.appendChild(button);
 
@@ -257,39 +274,23 @@ JSONEditor.defaults.themes.spectre = JSONEditor.AbstractTheme.extend({
   // Functions for rendering array with format: "table"
   getTable: function() {
     var el = this._super();
+    el.classList.add('table', 'table-scroll');
     if (this.options.table_border) el.classList.add('je-table-border');
-    return el;
-  },
-  getTableRow: function() {
-    var el = this._super();
-    if (this.options.table_border) el.classList.add('je-table-border');
-    if (this.options.table_zebrastyle) el.classList.add('je-table-zebra');
-    return el;
-  },
-  getTableHeaderCell: function(text) {
-    var el = this._super(text);
-    if (this.options.table_border) el.classList.add('je-table-border');
-    else if (this.options.table_hdiv) el.classList.add('je-table-hdiv');
-   return el;
-  },
-  getTableCell: function() {
-    var el = this._super();
-    if (this.options.table_border) el.classList.add('je-table-border');
-    else if (this.options.table_hdiv) el.classList.add('je-table-hdiv');
+    if (this.options.table_zebrastyle) el.classList.add('table-striped');
     return el;
   },
 
   // Containers for array with format: "tab"
   getTabHolder: function(propertyName) {
-    var pName = (typeof propertyName === 'undefined')? "" : propertyName;
+    var pName = typeof propertyName === 'undefined' ? "" : propertyName;
     var el = document.createElement('div');
     el.classList.add('columns');
-    el.innerHTML = '<div class="column col-2 btn-group tab"></div><div class="column col-10 content" id="' + pName + '"></div>';
+    el.innerHTML = '<div class="column col-2"></div><div class="column col-10 content" id="' + pName + '"></div>';
     return el;
   },
   // Containers for array with format: "tab-top"
   getTopTabHolder: function(propertyName) {
-    var pName = (typeof propertyName === 'undefined')? "" : propertyName;
+    var pName = typeof propertyName === 'undefined' ? "" : propertyName;
     var el = document.createElement('div');
     el.innerHTML = '<ul class="tab"></ul><div class="content" id="' + pName + '"></div>';
     return el;
@@ -343,14 +344,13 @@ JSONEditor.defaults.themes.spectre = JSONEditor.AbstractTheme.extend({
       input.errmsg = document.createElement('p');
       input.errmsg.classList.add('form-input-hint');
       input.controlgroup.appendChild(input.errmsg);
-    } else {
-      input.errmsg.style.display = '';
     }
+    input.errmsg.classList.remove('d-hide');
     input.errmsg.textContent = text;
   },
   removeInputError: function(input) {
     if (!input.errmsg) return;
-    input.errmsg.style.display = 'none';
+    input.errmsg.classList.add('d-hide');
     input.controlgroup.classList.remove('has-error');
   }
 
