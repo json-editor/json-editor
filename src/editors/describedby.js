8352f523ea8ec9e8656feab9b0f0ff0349ddce41
Peter Klein <pmk@io.dk>
2019-08-08 22:24:53 +0200

    minor fix

--- src/editors/describedby.js
+++ src/editors/describedby.js
@@ -85,8 +85,9 @@ JSONEditor.defaults.editors.describedBy = JSONEditor.AbstractEditor.extend({
 
     var schema = $extend({}, this.schema, this.jsoneditor.refs[ref]);
 
-    var editor = this.jsoneditor.createEditor(
-      this.jsoneditor.getEditorClass(schema), {
+    var editor_class = this.jsoneditor.getEditorClass(schema, this.jsoneditor);
+
+    var editor = this.jsoneditor.createEditor(editor_class, {
         jsoneditor: this.jsoneditor,
         schema: schema,
         container: holder,
