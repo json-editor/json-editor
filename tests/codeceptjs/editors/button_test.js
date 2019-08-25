b98d60b5776cee25bb9c29ab1b933aa3b7c837fa
Peter Klein <pmk@io.dk>
2019-08-25 17:08:26 +0200

    Tests for Button Editor

---
+++ tests/codeceptjs/editors/button_test.js
@@ -0,0 +1,24 @@
+var assert = require('assert');
+
+Feature('button');
+
+Scenario('should work with button editor callbacks', async (I) => {
+  I.amOnPage('button-callbacks.html');
+  I.seeElement('[data-schemapath="root.button1"] button');
+  I.click('[data-schemapath="root.button1"] button');
+  assert.equal(await I.grabValueFrom('.value'), 'button1CB');
+});
+
+Scenario('should work with button editor with option validated = true', async (I) => {
+  I.amOnPage('button-callbacks.html');
+  I.seeElement('[data-schemapath="root.button2"] button');
+  assert.equal(await I.grabAttributeFrom('[data-schemapath="root.button2"] button', 'disabled'), 'true');
+
+  I.seeElement('[data-schemapath="root.textinput"] input');
+  I.fillField('[name="root[textinput]"]', 'Hello World');
+  assert.equal(await I.grabAttributeFrom('[data-schemapath="root.button2"] button', 'disabled'), 'false');
+  I.seeElement('[data-schemapath="root.button2"] button');
+  I.click('[data-schemapath="root.button2"] button');
+  assert.equal(await I.grabValueFrom('.value'), 'button2CB');
+});
+
