e57dadd83d296b6be700b71ec73a844d00001205
Peter Klein <pmk@io.dk>
2019-08-31 21:49:35 +0200

    Fix of #514 - Multiselect didn't work with arrays

--- src/defaults.js
+++ src/defaults.js
@@ -411,7 +411,6 @@ JSONEditor.defaults.resolvers.unshift(function(schema) {
       // if 'selectize' enabled it is expected to be selectized control
       if (schema.format === "selectize") return 'arraySelectize';
       else if (schema.format === "select2") return 'arraySelect2';
-      else return 'multiselect'; // otherwise it is select
     }
   }
 });
