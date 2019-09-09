24d1c18e90d6e82b5592c17c7529e9bfcbcc7e06
Peter Klein <pmk@io.dk>
2019-09-09 20:48:55 +0200

    Moved progressbar functions to theme.js

--- src/themes/html.js
+++ src/themes/html.js
@@ -78,21 +78,5 @@ export var htmlTheme = AbstractTheme.extend({
       input.style.borderColor = '';
     }
     if(input.errmsg) input.errmsg.style.display = 'none';
-  },
-  getProgressBar: function() {
-    var max = 100, start = 0;
-
-    var progressBar = document.createElement('progress');
-    progressBar.setAttribute('max', max);
-    progressBar.setAttribute('value', start);
-    return progressBar;
-  },
-  updateProgressBar: function(progressBar, progress) {
-    if (!progressBar) return;
-    progressBar.setAttribute('value', progress);
-  },
-  updateProgressBarUnknown: function(progressBar) {
-    if (!progressBar) return;
-    progressBar.removeAttribute('value');
   }
 });
