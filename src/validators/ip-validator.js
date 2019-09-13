18ada665da494959027f9df37a4906e40e369939
Dave Sirockin <dave@flow-marine.com>
2019-09-13 10:32:28 +0200

    Added translation from @pmk65 ip.js

--- src/validators/ip-validator.js
+++ src/validators/ip-validator.js
@@ -1,43 +1,62 @@
-// Implements ipv4 and ipv6 format validations as perhttps://tools.ietf.org/html/draft-fge-json-schema-validation-00#section-7.3.4
-export function ipValidator(schema, value, path) {
-       var errors = [];
-       if(schema.format==="ipv4") 
-       {
-         try
-         {
-           var parts=value.split('.');
-           if( parts.length !==4)
-           {
-             throw(Error());
-           }
-           parts.forEach(function(part){
-             if( isNaN(+part) || +part < 0 || +part>255)
-             {
-               throw(Error());
-             }
-           });
-         }
-         catch(err)
-         {
-           errors.push({
-             path: path,
-             property: 'format',
-             message: 'ipv4 addresses must be in the form of 4 numbers between 0 and 255, separated by dots'
-           });
-         }
-       }
-       else if(schema.format==="ipv6")
-       {
-         if( !(/^(?:[A-F0-9]{1,4}:){7}[A-F0-9]{1,4}/).test(value))
-         {
-          errors.push({
-            path: path,
-            property: 'format',
-            message: 'ipv6 addresses must be in the form of 4 numbers between 0 and 255, separated by dots'
-          });  
-         }
-       }
-  
-       return errors;
-     }
-    
+// Implements ipv4, ipv6 and hostname format validations as per https://tools.ietf.org/html/draft-fge-json-schema-validation-00#section-7.3.4
+var validateIpv4 = function(value)
+{
+  var parts=value.split('.');
+  if( parts.length !==4)
+  {
+    throw(Error());
+  }
+  parts.forEach(function(part){
+    if( isNaN(+part) || +part < 0 || +part>255)
+    {
+      throw new Error('error_ipv4');
+    }
+  });
+};
+
+var validateIpv6 = function(value)
+{
+  if( !value.match(
+    '^(?:(?:(?:[a-fA-F0-9]{1,4}:){6}|(?=(?:[a-fA-F0-9]{0,4}:){2,6}(?:[0-9]{1,3}\.){3}[0-9]{1,3}$)(([0-9a-fA-F]{1,4}:){1,5}|:)((:[0-9a-fA-F]{1,4}){1,5}:|:)|::(?:[a-fA-F0-9]{1,4}:){5})(?:(?:25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\.){3}(?:25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])|(?:[a-fA-F0-9]{1,4}:){7}[a-fA-F0-9]{1,4}|(?=(?:[a-fA-F0-9]{0,4}:){0,7}[a-fA-F0-9]{0,4}$)(([0-9a-fA-F]{1,4}:){1,7}|:)((:[0-9a-fA-F]{1,4}){1,7}|:)|(?:[a-fA-F0-9]{1,4}:){7}:|:(:[a-fA-F0-9]{1,4}){7})$'    
+    ))
+  {
+   throw new Error('error_ipv6');
+  }
+};
+
+var validateHostname = function(value)
+{
+  if( !value.match(
+      '(?=^.{4,253}$)(^((?!-)[a-zA-Z0-9-]{0,62}[a-zA-Z0-9]\.)+[a-zA-Z]{2,63}$)'
+      ))
+    {
+      throw new Error('error_hostname');
+    }
+};
+
+export function ipValidator(schema, value, path, translate) {
+  try
+  {
+    switch( schema.format )
+    {
+      case "ipv4":
+        validateIpv4(value);
+        break;
+      case "ipv6":
+        validateIpv6(value);
+        break;
+      case "hostname":
+        validateHostname(value);
+        break;
+    }
+    return [];
+  }
+  catch(err)
+  {
+    return [{
+      path: path,
+      property: 'format',
+      message: translate(err.message)
+    }];
+  }
+}
