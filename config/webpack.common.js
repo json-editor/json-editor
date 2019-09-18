9b8cc4dba68ab0c03d50e5b16bcf0007f0c5c742
Dave Sirockin <dave@flow-marine.com>
2019-09-18 10:42:13 +0200

    removed `options.fix:true`

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -27,10 +27,7 @@ module.exports = {
         enforce: 'pre',
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: `eslint-loader`,
-        options: {
-          fix: true
-        }
+        loader: `eslint-loader`
       },
       {
         test: /\.css$/,
