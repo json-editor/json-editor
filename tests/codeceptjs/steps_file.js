8aaefb841c19f12613c1aeb6bfb3a6ffc82f4010
germanbisurgi <gebisurgi@gmail.com>
2019-10-17 16:03:16 +0200

    Fix for  #563 & #559 (pressKey not working enymore)

--- tests/codeceptjs/steps_file.js
+++ tests/codeceptjs/steps_file.js
@@ -7,6 +7,12 @@ module.exports = function() {
 
     // Define custom steps here, use 'this' to access default methods of I.
     // It is recommended to place a general 'login' function here.
-
+    pressKeys: function (string) {
+      this.wait(1)
+      let digits = string.split('')
+      digits.forEach((digit) => {
+        this.pressKey(digit)
+      })
+    }
   });
 }
