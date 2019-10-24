90578df18a5fbebd0afd30a3f5800f14eaa22008
germanbisurgi <gebisurgi@gmail.com>
2019-10-24 12:26:06 +0200

    integrated and homogeinized icons

--- src/iconlibs/fontawesome5.js
+++ src/iconlibs/fontawesome5.js
@@ -7,6 +7,7 @@ export var fontawesome5Iconlib = AbstractIconLib.extend({
     'delete': 'trash',
     edit: 'pen',
     add: 'plus',
+    subtract: 'minus',
     cancel: 'ban',
     save: 'save',
     moveup: 'arrow-up',
@@ -17,7 +18,7 @@ export var fontawesome5Iconlib = AbstractIconLib.extend({
     clear: 'times-circle',
     time: 'clock',
     calendar: 'calendar',
-    edit_properties: 'tasks'
+    edit_properties: 'list'
   },
   icon_prefix: 'fas fa-'
 })
