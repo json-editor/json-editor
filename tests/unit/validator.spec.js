f528c47a4c32bad8f77d1cfd15c636b42d004a37
tohosaku <tohosaku@users.osdn.me>
2020-02-13 14:59:20 +0900

    Add parameterize test to test Validator

--- tests/unit/validator.spec.js
+++ tests/unit/validator.spec.js
@@ -1,25 +1,93 @@
-/*
-Stub test file
-TODO: Write unit tests for all interfaces
-*/
+import { JSONEditor } from '../../src/core'
+import { Validator } from '../../src/validator'
+import { getDefaults } from '../../src/defaults'
+import fixture from '../fixtures/validation.json'
+import fixtureString from '../fixtures/string.json'
+import fixtureRecursive from '../fixtures/recursive.json'
+import * as math from 'mathjs'
+import { createFakeServer } from 'sinon'
 
-import { Validator } from '../../src/validator';
-import { getDefaults } from '../../src/defaults';
+describe('Validator', () => {
+  it('mathjs test', () => {
+    window.math = math
+    expect(window.math.mod(window.math.bignumber(1), window.math.bignumber(0.01))).toEqual(window.math.bignumber(0))
+  })
+  it('should exist', () => {
+    expect(Validator).toBeTruthy()
+  })
 
-describe("Validator", function() {
-  it("should exist", function(){
-    expect(Validator).toBeTruthy();
-  });
-  
-  describe("Constructor()", function(){
-      it("should create a new object", function(){
-        var jsonEditor = {};
-        var schema = {};
-        var options = {};
-        var defaults = getDefaults();
-        var instance = new Validator(jsonEditor, schema,options,defaults);
-        expect(instance).toBeTruthy();
-      });
-    });
-  
-});
+  describe('Constructor()', () => {
+    it('should create a new object', () => {
+      const jsonEditor = {}
+      const schema = {}
+      const options = {}
+      const defaults = getDefaults()
+      const instance = new Validator(jsonEditor, schema, options, defaults)
+      expect(instance).toBeTruthy()
+    })
+  })
+})
+
+describe('Validation Test', () => {
+  let element
+  let server
+
+  beforeAll(() => {
+    server = createFakeServer()
+    server.autoRespond = true
+    window.XMLHttpRequest = server.xhr
+    const response = {
+      'string': fixtureString,
+      'recursive': fixtureRecursive
+    }
+    server.respondWith(/(\w+)\.json/, (xhr, name) => {
+      xhr.respond(200, { 'Content-Type': 'application/json' }, JSON.stringify(response[name]))
+    })
+  })
+
+  afterAll(() => {
+    server.restore()
+  })
+
+  beforeEach(() => {
+    window.math = math
+    document.body.insertAdjacentHTML(
+      'afterbegin',
+      '<div id="fixture"></div>')
+    element = document.getElementById('fixture')
+  })
+
+  Object.entries(fixture).forEach(([subject, spec]) => {
+    describe(subject, () => {
+      let editor
+      beforeEach(() => {
+        editor = new JSONEditor(element, {
+          schema: spec.schema,
+          ajax: true
+        })
+      })
+
+      afterEach(() => {
+        editor.destroy()
+      })
+      spec.valid.forEach((v, i) => {
+        it(`valid data ${i + 1}`, (done) => {
+          editor.on('ready', () => {
+            const result = editor.validate(v)
+            expect(result.length).toBe(0)
+            done()
+          })
+        })
+      })
+      spec.invalid.forEach((v, i) => {
+        it(`invalid data ${i + 1}`, (done) => {
+          editor.on('ready', () => {
+            const result = editor.validate(v)
+            expect(result.length).toBeGreaterThan(0)
+            done()
+          })
+        })
+      })
+    })
+  })
+})
