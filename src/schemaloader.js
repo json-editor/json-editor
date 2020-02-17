f68cbb328200b9156bae26f721db771362e6f8e3
tohosaku <tohosaku@users.osdn.me>
2020-02-17 09:20:56 +0900

    Split Schemaloader#expandSchema

--- src/schemaloader.js
+++ src/schemaloader.js
@@ -38,70 +38,90 @@ export const SchemaLoader = Class.extend({
     return this.extendSchemas(_schema, this.expandSchema(this.refs[ref]))
   },
   expandSchema: function (schema, fileBase) {
-    var self = this
-    var extended = $extend({}, schema)
-    var i
-
-    // Version 3 `type`
-    if (typeof schema.type === 'object') {
-      schema.type = this._expandSubSchema(schema.type)
-    }
-
-    // Version 3 `disallow`
-    if (typeof schema.disallow === 'object') {
-      schema.disallow = this._expandSubSchema(schema.disallow)
-    }
-
-    // Version 4 `anyOf`
-    if (schema.anyOf) {
-      $each(schema.anyOf, function (key, value) {
-        schema.anyOf[key] = self.expandSchema(value)
-      })
-    }
-    // Version 4 `dependencies` (schema dependencies)
-    if (schema.dependencies) {
-      $each(schema.dependencies, function (key, value) {
-        if (typeof value === 'object' && !(Array.isArray(value))) {
-          schema.dependencies[key] = self.expandSchema(value)
-        }
-      })
-    }
-    // Version 4 `not`
-    if (schema.not) {
-      schema.not = this.expandSchema(schema.not)
-    }
-
-    // allOf schemas should be merged into the parent
-    if (schema.allOf) {
-      for (i = 0; i < schema.allOf.length; i++) {
-        schema.allOf[i] = this.expandRefs(schema.allOf[i], true)
-        extended = this.extendSchemas(extended, this.expandSchema(schema.allOf[i]))
+    Object.entries(this._subSchema1).forEach(([key, func]) => {
+      if (schema[key]) {
+        func.call(this, schema)
       }
-      delete extended.allOf
-    }
-    // extends schemas should be merged into parent
-    if (schema.extends) {
-      // If extends is a schema
-      if (!(Array.isArray(schema.extends))) {
-        extended = this.extendSchemas(extended, this.expandSchema(schema.extends))
-      } else {
-        // If extends is an array of schemas
-        for (i = 0; i < schema.extends.length; i++) {
-          extended = this.extendSchemas(extended, this.expandSchema(schema.extends[i]))
-        }
+    })
+
+    let extended = $extend({}, schema)
+
+    Object.entries(this._subSchema2).forEach(([key, func]) => {
+      if (schema[key]) {
+        extended = func.call(this, schema, extended)
       }
-      delete extended.extends
-    }
-    // parent should be merged into oneOf schemas
-    if (schema.oneOf) {
-      var tmp = $extend({}, extended)
-      delete tmp.oneOf
-      for (i = 0; i < schema.oneOf.length; i++) {
-        extended.oneOf[i] = this.extendSchemas(this.expandSchema(schema.oneOf[i]), tmp)
-      }
-    }
+    })
 
     return this.expandRefs(extended)
+  },
+  _subSchema1: {
+    // Version 3 `type`
+    type: function (schema) {
+      if (typeof schema.type === 'object') {
+        schema.type = this._expandSubSchema(schema.type)
+      }
+    },
+    // Version 3 `disallow`
+    disallow: function (schema) {
+      if (typeof schema.disallow === 'object') {
+        schema.disallow = this._expandSubSchema(schema.disallow)
+      }
+    },
+    // Version 4 `anyOf`
+    anyOf: function (schema) {
+      $each(schema.anyOf, (key, value) => {
+        schema.anyOf[key] = this.expandSchema(value)
+      })
+    },
+    // Version 4 `dependencies` (schema dependencies)
+    dependencies: function (schema) {
+      $each(schema.dependencies, (key, value) => {
+        if (typeof value === 'object' && !(Array.isArray(value))) {
+          schema.dependencies[key] = this.expandSchema(value)
+        }
+      })
+    },
+    // Version 4 `not`
+    not: function (schema) {
+      schema.not = this.expandSchema(schema.not)
+    }
+  },
+  _subSchema2: {
+    // allOf schemas should be merged into the parent
+    allOf: function (schema, extended) {
+      let _extended = $extend({}, extended)
+      for (let i = 0; i < schema.allOf.length; i++) {
+        schema.allOf[i] = this.expandRefs(schema.allOf[i], true)
+        _extended = this.extendSchemas(_extended, this.expandSchema(schema.allOf[i]))
+      }
+      delete _extended.allOf
+      return _extended
+    },
+    // extends schemas should be merged into parent
+    extends: function (schema, extended) {
+      let _extended
+      // If extends is a schema
+      if (!(Array.isArray(schema.extends))) {
+        _extended = this.extendSchemas(extended, this.expandSchema(schema.extends))
+      } else {
+        // If extends is an array of schemas
+        _extended = schema.extends.reduce((e, s, i) => {
+          return this.extendSchemas(e, this.expandSchema(s))
+        }, extended)
+      }
+      delete _extended.extends
+      return _extended
+    },
+    // parent should be merged into oneOf schemas
+    oneOf: function (schema, extended) {
+      const tmp = $extend({}, extended)
+      delete tmp.oneOf
+      schema.oneOf.reduce((e, s, i) => {
+        e.oneOf[i] = this.extendSchemas(this.expandSchema(s), tmp)
+        return e
+      }, extended)
+      return extended
+    }
   },
   _getRef: function (fetchUrl, refObj) {
     var ref = fetchUrl + refObj
@@ -246,60 +266,63 @@ export const SchemaLoader = Class.extend({
     obj1 = $extend({}, obj1)
     obj2 = $extend({}, obj2)
 
-    var self = this
-    var extended = {}
-    $each(obj1, function (prop, val) {
+    const extended = {}
+    const isRequiredOrDefaultProperties = (prop, val) => (prop === 'required' || prop === 'defaultProperties') && typeof val === 'object' && Array.isArray(val)
+    const merge = (prop, val) => {
+      // Required and defaultProperties arrays should be unioned together
+      if (isRequiredOrDefaultProperties(prop, val)) {
+        // Union arrays and unique
+        extended[prop] = val.concat(obj2[prop]).reduce((p, c) => {
+          if (p.indexOf(c) < 0) p.push(c)
+          return p
+        }, [])
+      } else if (prop === 'type' && (typeof val === 'string' || Array.isArray(val))) {
+        mergeType(val)
+      } else if (typeof val === 'object' && !Array.isArray(val) && val !== null) {
+        // Objects should be recursively merged
+        extended[prop] = this.extendSchemas(val, obj2[prop])
+      } else {
+        // Otherwise, use the first value
+        extended[prop] = val
+      }
+    }
+    const mergeType = (val) => {
+      // Type should be intersected and is either an array or string
+      // Make sure we're dealing with arrays
+      if (typeof val === 'string') val = [val]
+      if (typeof obj2.type === 'string') obj2.type = [obj2.type]
+
+      // If type is only defined in the first schema, keep it
+      if (!obj2.type || !obj2.type.length) {
+        extended.type = val
+      } else {
+        // If type is defined in both schemas, do an intersect
+        extended.type = val.filter(n => obj2.type.indexOf(n) !== -1)
+      }
+
+      // If there's only 1 type and it's a primitive, use a string instead of array
+      if (extended.type.length === 1 && typeof extended.type[0] === 'string') {
+        extended.type = extended.type[0]
+      } else if (extended.type.length === 0) {
+        // Remove the type property if it's empty
+        delete extended.type
+      }
+    }
+    $each(obj1, (prop, val) => {
       // If this key is also defined in obj2, merge them
       if (typeof obj2[prop] !== 'undefined') {
-        // Required and defaultProperties arrays should be unioned together
-        if ((prop === 'required' || prop === 'defaultProperties') && typeof val === 'object' && Array.isArray(val)) {
-          // Union arrays and unique
-          extended[prop] = val.concat(obj2[prop]).reduce(function (p, c) {
-            if (p.indexOf(c) < 0) p.push(c)
-            return p
-          }, [])
-        } else if (prop === 'type' && (typeof val === 'string' || Array.isArray(val))) {
-          // Type should be intersected and is either an array or string
-          // Make sure we're dealing with arrays
-          if (typeof val === 'string') val = [val]
-          if (typeof obj2.type === 'string') obj2.type = [obj2.type]
-
-          // If type is only defined in the first schema, keep it
-          if (!obj2.type || !obj2.type.length) {
-            extended.type = val
-          } else {
-            // If type is defined in both schemas, do an intersect
-            extended.type = val.filter(function (n) {
-              return obj2.type.indexOf(n) !== -1
-            })
-          }
-
-          // If there's only 1 type and it's a primitive, use a string instead of array
-          if (extended.type.length === 1 && typeof extended.type[0] === 'string') {
-            extended.type = extended.type[0]
-          } else if (extended.type.length === 0) {
-            // Remove the type property if it's empty
-            delete extended.type
-          }
-        } else if (typeof val === 'object' && !Array.isArray(val) && val !== null) {
-          // Objects should be recursively merged
-          extended[prop] = self.extendSchemas(val, obj2[prop])
-        } else {
-          // Otherwise, use the first value
-          extended[prop] = val
-        }
+        merge(prop, val)
       } else {
         // Otherwise, just use the one in obj1
         extended[prop] = val
       }
     })
     // Properties in obj2 that aren't in obj1
-    $each(obj2, function (prop, val) {
+    $each(obj2, (prop, val) => {
       if (typeof obj1[prop] === 'undefined') {
         extended[prop] = val
       }
     })
-
     return extended
   }
 })
