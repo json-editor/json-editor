99da489d8b55bc8f01fe12cd74ab662d1e7cb0b8
Adnan <adnan@websemantics.ca>
2020-03-10 08:34:19 -0400

    Fixed defaults.spec.js tests

--- tests/unit/defaults.spec.js
+++ tests/unit/defaults.spec.js
@@ -3,18 +3,14 @@ Stub test file
 TODO: Write unit tests for all interfaces
 */
 
-import { getDefaults } from '../../src/defaults';
+import { defaults } from '../../src/defaults';
 
 
 describe("defaults", function() {
-  it("should be a function", function(){
-    expect(typeof getDefaults).toBe("function");
-  });  
-  it("should return an object", function(){
-    expect(typeof(getDefaults())).toBe("object");
+  it("should be an object", function(){
+    expect(typeof defaults).toBe("object");
   });
   it("should have standard properties defined", function(){
-    let defaults = getDefaults();
     expect(typeof(defaults.themes)).toBe("object");
     expect(typeof(defaults.templates)).toBe("object");
     expect(typeof(defaults.iconlibs)).toBe("object");
