7b3d0b5364e0534a08a4551197a19d669e661b0a
German Bisurgi <g.bisurgi@herzogkommunikation.de>
2020-05-11 14:39:40 +0200

    updated unique id

--- src/themes/bootstrap4.js
+++ src/themes/bootstrap4.js
@@ -126,7 +126,7 @@ export class bootstrap4Theme extends AbstractTheme {
         }
       }
 
-      const unique = label.innerText + '-' + (Date.now() * Math.random())
+      const unique = (Date.now() * Math.random())
       input.setAttribute('id', unique)
       label.setAttribute('for', unique)
 
