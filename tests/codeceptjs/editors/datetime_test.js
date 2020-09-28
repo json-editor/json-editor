6dcffb01a67beb10f70ba0d45059145f85ee8257
German Bisurgi <g.bisurgi@herzogkommunikation.de>
2020-09-28 09:23:34 +0200

    updated datetime test

--- tests/codeceptjs/editors/datetime_test.js
+++ tests/codeceptjs/editors/datetime_test.js
@@ -1,4 +1,4 @@
-/* global Feature Scenario Event */
+/* global Feature Scenario */
 
 const assert = require('assert')
 
@@ -23,65 +23,11 @@ Scenario('time should have max and min attributes @datetime', async (I) => {
 })
 
 Scenario('date should have max and min attributes @datetime', async (I) => {
-  assert.equal(await I.grabAttributeFrom('[name="root[date]"]', 'min'), '2020-09-16')
-  assert.equal(await I.grabAttributeFrom('[name="root[date]"]', 'max'), '2020-09-20')
+  assert.equal(await I.grabAttributeFrom('[name="root[date]"]', 'min'), '1970-01-01')
+  assert.equal(await I.grabAttributeFrom('[name="root[date]"]', 'max'), '2100-01-01')
 })
 
 Scenario('datetimelocal should have max and min attributes @datetime', async (I) => {
-  assert.equal(await I.grabAttributeFrom('[name="root[datetimelocal]"]', 'min'), '2020-09-16T00:00')
-  assert.equal(await I.grabAttributeFrom('[name="root[datetimelocal]"]', 'max'), '2020-09-20T00:00')
-})
-
-Scenario('time should be limited to min and max attributes @optional', async (I) => {
-  I.executeScript(() => {
-    const time = document.querySelector('[name="root[time]"]')
-    time.stepUp(1000000)
-    time.dispatchEvent(new Event('change', { bubbles: true, cancelable: true }))
-  })
-  I.click('#get-value')
-  assert.equal(await I.grabValueFrom('#value'), '{"time":"13:00","date":"' + today() + '","datetimelocal":"' + today() + 'T00:00' + '"}')
-
-  I.executeScript(() => {
-    const time = document.querySelector('[name="root[time]"]')
-    time.stepDown(2000000)
-    time.dispatchEvent(new Event('change', { bubbles: true, cancelable: true }))
-  })
-  I.click('#get-value')
-  assert.equal(await I.grabValueFrom('#value'), '{"time":"11:00","date":"' + today() + '","datetimelocal":"' + today() + 'T00:00' + '"}')
-})
-
-Scenario('date should be limited to min and max attributes @optional', async (I) => {
-  I.executeScript(() => {
-    const datetime = document.querySelector('[name="root[date]"]')
-    datetime.stepUp(100)
-    datetime.dispatchEvent(new Event('change', { bubbles: true, cancelable: true }))
-  })
-  I.click('#get-value')
-  assert.equal(await I.grabValueFrom('#value'), '{"time":"11:00","date":"' + today(2) + '","datetimelocal":"' + today() + 'T00:00' + '"}')
-
-  I.executeScript(() => {
-    const datetime = document.querySelector('[name="root[date]"]')
-    datetime.stepDown(100)
-    datetime.dispatchEvent(new Event('change', { bubbles: true, cancelable: true }))
-  })
-  I.click('#get-value')
-  assert.equal(await I.grabValueFrom('#value'), '{"time":"11:00","date":"' + today(-2) + '","datetimelocal":"' + today() + 'T00:00' + '"}')
-})
-
-Scenario('datetimelocal should be limited to min and max attributes @optional', async (I) => {
-  I.executeScript(() => {
-    const datetimelocal = document.querySelector('[name="root[datetimelocal]"]')
-    datetimelocal.stepUp(1000000)
-    datetimelocal.dispatchEvent(new Event('change', { bubbles: true, cancelable: true }))
-  })
-  I.click('#get-value')
-  assert.equal(await I.grabValueFrom('#value'), '{"time":"11:00","date":"' + today(-2) + '","datetimelocal":"' + today(2) + 'T00:00' + '"}')
-
-  I.executeScript(() => {
-    const datetimelocal = document.querySelector('[name="root[datetimelocal]"]')
-    datetimelocal.stepDown(1000000)
-    datetimelocal.dispatchEvent(new Event('change', { bubbles: true, cancelable: true }))
-  })
-  I.click('#get-value')
-  assert.equal(await I.grabValueFrom('#value'), '{"time":"11:00","date":"' + today(-2) + '","datetimelocal":"' + today(-2) + 'T00:00' + '"}')
+  assert.equal(await I.grabAttributeFrom('[name="root[datetimelocal]"]', 'min'), '1970-01-01T00:00')
+  assert.equal(await I.grabAttributeFrom('[name="root[datetimelocal]"]', 'max'), '2100-01-01T00:00')
 })
