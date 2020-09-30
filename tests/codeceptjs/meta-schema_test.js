24b001fda0800f0773846a5885c4951461efe359
Tobias Munk <schmunk@usrbin.de>
2020-09-30 07:44:16 +0200

    hardened test

--- tests/codeceptjs/meta-schema_test.js
+++ tests/codeceptjs/meta-schema_test.js
@@ -5,7 +5,7 @@ Feature('core');
 // https://github.com/json-editor/json-editor/issues/823
 Scenario('work with json-schema meta-schema', async (I) => {
   I.amOnPage('issues/issue-gh-823-meta-schema.html');
-  I.waitForElement('[data-schemapath="root"] h3');
+  I.waitForElement('[data-schemapath="root"] h3', 10);
   I.click('Object Properties');
   I.click('options');
   I.see('$ref');
