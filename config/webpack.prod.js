9e554233a001beeb150743b385e21dabb3c1af93
German Bisurgi <g.bisurgi@herzogkommunikation.de>
2020-10-16 12:55:01 +0200

    fixed babel transpiling bug. (Babel were not working at all since 30.06.2020 after trying to merge webpack configs). Also introduced the use of polyfills when required (corejs 3). Set Hints false (too much output)

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -5,32 +5,6 @@ const commonConfig = require('./webpack.common.js')
 const helpers = require('./helpers')
 
 module.exports = () => {
-  commonConfig.module.rules = [
-    {
-      enforce: 'pre',
-      test: /\.js$/,
-      exclude: /node_modules/,
-      loader: 'eslint-loader'
-    },
-    {
-      test: /\.css$/,
-      use: [
-        'style-loader',
-        {
-          loader: 'postcss-loader',
-          options: {
-            sourceMap: true,
-            plugins: [
-              require('cssnano')({
-                preset: 'default'
-              })
-            ]
-          }
-        }
-      ]
-    }
-  ]
-
   return webpackMerge(commonConfig, {
     mode: 'production',
     output: {
@@ -39,11 +13,9 @@ module.exports = () => {
       filename: '[name].js',
       libraryTarget: 'umd'
     },
-
     optimization: {
       minimize: true
     },
-
     plugins: [
       new RemoveStrictPlugin(), // I have put this in to avoid IE throwing error Assignment to read-only properties is not allowed in strict mode
       // This doesn't seem to actually be minimising the CSS!
