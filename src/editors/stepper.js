0ae1d06109607822c0c7b8cca3f1c9f4a240fd4d
German Bisurgi <13135260+germanbisurgi@users.noreply.github.com>
2021-01-12 15:13:25 +0100

    fix stepper safari bug (#894)
    Co-authored-by: g.bisurgi <g.bisurgi@herzogkommunikation.de>

--- src/editors/stepper.js
+++ src/editors/stepper.js
@@ -4,6 +4,9 @@ export class StepperEditor extends IntegerEditor {
   build () {
     super.build()
     this.input.setAttribute('type', 'number')
+    if (!this.input.getAttribute('step')) {
+      this.input.setAttribute('step', '1')
+    }
     const stepperButtons = this.theme.getStepperButtons(this.input)
     this.control.appendChild(stepperButtons)
     this.stepperDown = this.control.querySelector('.stepper-down')
