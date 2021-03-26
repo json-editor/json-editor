94a6dbbbd068c84e8d3f3b470fcb5ecf751f3cd6
g.bisurgi <g.bisurgi@herzogkommunikation.de>
2021-03-26 17:02:33 +0100

    fix: infoText not working with bootstrap3 #919

--- src/themes/bootstrap3.js
+++ src/themes/bootstrap3.js
@@ -50,7 +50,7 @@ export class bootstrap3Theme extends AbstractTheme {
     return el
   }
 
-  getFormControl (label, input, description) {
+  getFormControl (label, input, description, infoText) {
     const group = document.createElement('div')
 
     if (label && (input.type === 'checkbox' || input.type === 'radio')) {
@@ -62,6 +62,7 @@ export class bootstrap3Theme extends AbstractTheme {
       if (label) {
         label.classList.add('control-label')
         group.appendChild(label)
+        if (infoText) label.appendChild(infoText)
       }
       group.appendChild(input)
     }
@@ -93,8 +94,7 @@ export class bootstrap3Theme extends AbstractTheme {
     tooltip.style.color = '#FAFAFA'
     tooltip.style.padding = '.5rem 1rem'
     tooltip.style['border-radius'] = '.25rem'
-    tooltip.style.width = '25rem'
-    tooltip.style.transform = 'translateX(-27rem) translateY(-.5rem)'
+    tooltip.style.width = '50vw'
     tooltip.style.position = 'absolute'
     tooltip.innerText = text
     icon.onmouseover = () => {
