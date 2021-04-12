0f023fad2698f1a288b876cc7b9e19d877faa366
g.bisurgi <g.bisurgi@herzogkommunikation.de>
2021-04-12 16:43:08 +0200

    updated test to comply with es6 and linting

--- tests/codeceptjs/editors/issues/issue-gh-812_test.js
+++ tests/codeceptjs/editors/issues/issue-gh-812_test.js
@@ -1,31 +1,32 @@
-var assert = require('assert');
+/* global Feature Scenario */
 
-Feature('issues');
+const assert = require('assert')
+let value
 
-Scenario('GitHub issue 812 should remain fixed', async (I) => {
-  I.amOnPage('issues/issue-gh-812.html');
+Feature('issues')
 
-  I.click('.get-value');
-  value = await I.grabValueFrom('.debug');
-  assert.equal(value, '{"students":[{"name":"AAA","sessions":[{"student_name":"AAA","minutes":15},{"student_name":"AAA","minutes":15}]},{"name":"BBB","sessions":[{"student_name":"BBB","minutes":20}]},{"name":"CCC","sessions":[{"student_name":"CCC","minutes":10}]}]}');
+Scenario('GitHub issue 812 should remain fixed @issue-812', async (I) => {
+  I.amOnPage('issues/issue-gh-812.html')
 
-  I.amAcceptingPopups();
-  I.click('//*[@id="root.students.0"]/span[2]/button[contains(@class, "json-editor-btn-delete") and @data-i="0"]');
-  I.seeInPopup('Are you sure you want to remove this node?');
-  I.acceptPopup();
+  I.click('.get-value')
+  value = await I.grabValueFrom('.debug')
+  assert.equal(value, '{"students":[{"name":"AAA","sessions":[{"student_name":"AAA","minutes":15},{"student_name":"AAA","minutes":15}]},{"name":"BBB","sessions":[{"student_name":"BBB","minutes":20}]},{"name":"CCC","sessions":[{"student_name":"CCC","minutes":10}]}]}')
 
-  I.click('.get-value');
-  value = await I.grabValueFrom('.debug');
-  assert.equal(value, '{"students":[{"name":"BBB","sessions":[{"student_name":"BBB","minutes":20}]},{"name":"CCC","sessions":[{"student_name":"CCC","minutes":10}]}]}');
+  I.amAcceptingPopups()
+  I.click('//*[@id="root.students.0"]/span[2]/button[contains(@class, "json-editor-btn-delete") and @data-i="0"]')
+  I.seeInPopup('Are you sure you want to remove this node?')
+  I.acceptPopup()
 
-  I.amAcceptingPopups();
-  I.click('//*[@id="root.students.0"]/span[2]/button[contains(@class, "json-editor-btn-delete") and @data-i="0"]');
-  I.seeInPopup('Are you sure you want to remove this node?');
-  I.acceptPopup();
+  I.click('.get-value')
+  value = await I.grabValueFrom('.debug')
+  assert.equal(value, '{"students":[{"name":"BBB","sessions":[{"student_name":"BBB","minutes":20}]},{"name":"CCC","sessions":[{"student_name":"CCC","minutes":10}]}]}')
 
-  I.click('.get-value');
-  value = await I.grabValueFrom('.debug');
-  assert.equal(value, '{"students":[{"name":"CCC","sessions":[{"student_name":"CCC","minutes":10}]}]}');
-});
+  I.amAcceptingPopups()
+  I.click('//*[@id="root.students.0"]/span[2]/button[contains(@class, "json-editor-btn-delete") and @data-i="0"]')
+  I.seeInPopup('Are you sure you want to remove this node?')
+  I.acceptPopup()
 
-
+  I.click('.get-value')
+  value = await I.grabValueFrom('.debug')
+  assert.equal(value, '{"students":[{"name":"CCC","sessions":[{"student_name":"CCC","minutes":10}]}]}')
+})
