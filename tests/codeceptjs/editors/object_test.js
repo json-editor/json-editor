279974090ee7161e75668a5c4c76a98c60bc0edd
Nickolay Zubov <nickrus@yandex.ru>
2021-05-10 12:58:11 +0300

    fix extra test for show-non-required-editors=false

--- tests/codeceptjs/editors/object_test.js
+++ tests/codeceptjs/editors/object_test.js
@@ -210,7 +210,7 @@ Scenario('should show editors for empty optional fields if show_non_required_edi
 })
 
 Scenario('should NOT show editors for empty optional fields if show_non_required_editors options is false', (I) => {
-  I.amOnPage('object-show-non-required-editors--true.html')
+  I.amOnPage('object-show-non-required-editors--false.html')
   I.seeElement('[name="root[0][dayCode]"]')
   I.dontSeeElement('[name="root[0][startTime]"]')
 })
