584d8529f95e6d12160964d3860a89f2fe7439c8
Alexis Boni <mail@aboni.dev>
2022-03-04 13:12:52 -0300

    Add more uuid tests

--- tests/codeceptjs/editors/uuid_test.js
+++ tests/codeceptjs/editors/uuid_test.js
@@ -12,10 +12,37 @@ Scenario('should have initial value matching uuid @uuid', async (I) => {
 })
 
 Scenario('should have initial value matching uuid in arrays @uuid', async (I) => {
-  I.click('Add item')
-  I.click('Add item')
-  const value0 = await I.grabValueFrom('[name="root[uuidArray][0][uuid]"]')
+  I.click('Add uuid string array item')
+  I.click('Add uuid string array item')
+  const value0 = await I.grabValueFrom('[name="root[uuidStringArray][0]"]')
   assert.equal((/^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i.test(value0)), true)
-  const value1 = await I.grabValueFrom('[name="root[uuidArray][1][uuid]"]')
+  const value1 = await I.grabValueFrom('[name="root[uuidStringArray][1]"]')
+  assert.equal((/^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i.test(value1)), true)
+})
+
+Scenario('should have initial value matching uuid in arrays of objects with @uuid', async (I) => {
+  I.click('Add uuid object array item')
+  I.click('Add uuid object array item')
+  const value0 = await I.grabValueFrom('[name="root[uuidObjectArray][0][uuid]"]')
+  assert.equal((/^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i.test(value0)), true)
+  const value1 = await I.grabValueFrom('[name="root[uuidObjectArray][1][uuid]"]')
+  assert.equal((/^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i.test(value1)), true)
+})
+
+Scenario('should have initial value matching uuid in arrays (table) of strings with @uuid', async (I) => {
+  I.click('Add uuid string table item')
+  I.click('Add uuid string table item')
+  const value0 = await I.grabValueFrom('[name="root[uuidStringTable][0]')
+  assert.equal((/^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i.test(value0)), true)
+  const value1 = await I.grabValueFrom('[name="root[uuidStringTable][1]')
+  assert.equal((/^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i.test(value1)), true)
+})
+
+Scenario('should have initial value matching uuid in arrays (table) of objects with @uuid', async (I) => {
+  I.click('Add uuid object table item')
+  I.click('Add uuid object table item')
+  const value0 = await I.grabValueFrom('[name="root[uuidObjectTable][0][uuid]"]')
+  assert.equal((/^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i.test(value0)), true)
+  const value1 = await I.grabValueFrom('[name="root[uuidObjectTable][1][uuid]"]')
   assert.equal((/^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i.test(value1)), true)
 })
