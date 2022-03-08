1022843643a6f7d07bfe77716fe24b993d9157b4
g.bisurgi <g.bisurgi@herzogkommunikation.de>
2022-03-08 14:56:58 +0100

    updated test

--- tests/codeceptjs/editors/autocomplete_test.js
+++ tests/codeceptjs/editors/autocomplete_test.js
@@ -6,7 +6,7 @@ Feature('autocomplete')
 
 Scenario('autocomplete should work @autocomplete', async (I) => {
   I.amOnPage('autocomplete.html')
-  I.waitForElement('.je-ready')
+  I.waitForElement('.je-ready', 5)
   I.fillField('root', 'ir')
   I.waitForText('iran', 5, '.autocomplete-result-list')
   I.waitForText('iraq', 5, '.autocomplete-result-list')
