dccc735cf074ab72cd92480aaa41e9ef5b82bd08
g.bisurgi <g.bisurgi@herzogkommunikation.de>
2022-03-08 14:47:16 +0100

    Fix for autocomplete (#1124) + test

---
+++ tests/codeceptjs/editors/autocomplete_test.js
@@ -0,0 +1,15 @@
+/* global Feature Scenario */
+
+var assert = require('assert')
+
+Feature('autocomplete')
+
+Scenario('autocomplete should work @autocomplete', async (I) => {
+  I.amOnPage('autocomplete.html')
+  I.waitForElement('.je-ready')
+  I.fillField('root', 'ir')
+  I.waitForText('iran', 5, '.autocomplete-result-list')
+  I.waitForText('iraq', 5, '.autocomplete-result-list')
+  I.click('iraq', '.autocomplete-result:nth-child(2)')
+  assert.equal(await I.grabValueFrom('.value'), '"iraq"')
+})
