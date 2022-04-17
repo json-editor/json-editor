d2686b134eb8648c4788370dc64513f7c15dd0f2
Yeonhobo <yeonhojang@gmail.com>
2022-04-17 16:28:41 -0400

    April 17, 2022. Added a Jasmine Unit test for pattern dependency.

--- tests/unit/editors/object.spec.js
+++ tests/unit/editors/object.spec.js
@@ -53,6 +53,58 @@ const fixture = [
   }
 ]
 
+const patternDepFixture = {
+  type: "object",
+  title: "Pattern Dependency",
+  required: [
+    "ServerName"
+  ],
+  properties: {
+    ServerName: {
+      type: "string",
+      title: "Server Name",
+      pattern: "^(AA|BB|CC)-[0-99]{2}$",
+    },
+    Cloud1: {
+      title: "Cloud1",
+      type: "string",
+      enum: ["one", "two"],
+      options: {
+        dependencies: {
+          ServerName: {
+            pattern: "^(AA)-[0-99]{2}$"
+          }
+        }
+        
+      }
+    },
+    Cloud2: {
+      title: "Cloud2",
+      type: "string",
+      enum: ["three", "four"],
+      options: {
+        dependencies: {
+          ServerName: {
+           pattern: "^(BB)-[0-99]{2}$"
+          }
+        }
+      }
+    },
+    Cloud3: {
+      title: "Cloud3",
+      type: "string",
+      enum: ["five", "six"],
+      options: {
+        dependencies: {
+          ServerName: {
+            pattern: "^(CC)-[0-99]{2}$"
+          }
+        }
+      }
+    }
+  }
+}
+
 describe('Object Editor', () => {
   let element
   let editor
@@ -77,5 +129,63 @@ describe('Object Editor', () => {
         expect(editor.getValue()).toEqual(spec.value)
       })
     })
+  })
+
+})
+
+describe('Object Editor on Pattern Dependency', () => {
+  let element
+  let editor
+
+  beforeEach(() => {
+    document.body.insertAdjacentHTML(
+      'afterbegin',
+      '<div id="patternDepFixture"></div>')
+    element = document.getElementById('patternDepFixture')
+  })
+
+  afterEach(() => {
+    editor.destroy()
+  })
+
+  
+  it('testing appearance of Cloud1 due to patterns', () =>{
+    editor = new JSONEditor(element, {
+      schema: patternDepFixture
+    })
+    editor.promise.then(() => {
+      const ServerName = editor.getEditor('root.ServerName');
+      ServerName.setValue("AA-01");
+      expect(editor.getValue()["Cloud1"]).toBeDefined()
+      expect(editor.getValue()["Cloud2"]).toBeUndefined()
+      expect(editor.getValue()["Cloud3"]).toBeUndefined()
+    })
+
+  })
+  it('testing appearance of Cloud2 due to patterns', () =>{
+    editor = new JSONEditor(element, {
+      schema: patternDepFixture
+    })
+    editor.promise.then(() => {
+      const ServerName = editor.getEditor('root.ServerName');
+      ServerName.setValue("BB-01");
+      expect(editor.getValue()["Cloud2"]).toBeDefined()
+      expect(editor.getValue()["Cloud1"]).toBeUndefined()
+      expect(editor.getValue()["Cloud3"]).toBeUndefined()
+    })
+
+  })
+  it('testing appearance of Cloud3 due to patterns', () =>{
+    editor = new JSONEditor(element, {
+      schema: patternDepFixture
+    })
+    editor.promise.then(() => {
+      const ServerName = editor.getEditor('root.ServerName');
+      ServerName.setValue("CC-01");
+      expect(editor.getValue()["Cloud3"]).toBeDefined()
+      expect(editor.getValue()["Cloud1"]).toBeUndefined()
+      expect(editor.getValue()["Cloud2"]).toBeUndefined()
+    })
+
   })
 })
