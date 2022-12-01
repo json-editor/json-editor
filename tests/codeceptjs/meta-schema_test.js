ebd84daf9ea8f99905d0b7fc2239681db1120866
German Bisurgi <gebisurgi@protonmail.com>
2022-12-01 16:04:26 +0100

    Passing and failing meta-schema texts examples

--- tests/codeceptjs/meta-schema_test.js
+++ tests/codeceptjs/meta-schema_test.js
@@ -17,3 +17,17 @@ Scenario('work with json-schema meta-schema @meta-schema', async (I) => {
   I.dontSee('Value must validate against exactly one of the provided schemas. It currently validates against 0 of the schemas.')
   I.dontSee('No additional properties allowed, but property title is set')
 })
+
+// https://github.com/json-editor/json-editor/issues/1233
+Scenario('passing meta-schema example @meta-schema', async (I) => {
+  I.amOnPage('issues/issue-gh-1233-passing.html')
+  I.waitForElement('.je-ready', DEFAULT_WAIT_TIME)
+  I.waitForElement('[name="root[name]"]', DEFAULT_WAIT_TIME)
+})
+
+// https://github.com/json-editor/json-editor/issues/1233
+Scenario('failing meta-schema example  @meta-schema @optional', async (I) => {
+  I.amOnPage('issues/issue-gh-1233-failing.html')
+  I.waitForElement('.je-ready', DEFAULT_WAIT_TIME)
+  I.waitForElement('[name="root[name]"]', DEFAULT_WAIT_TIME)
+})
