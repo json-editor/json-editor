f1369ca570e9fba64b06788bd148e644e5ca5764
German Bisurgi <gebisurgi@protonmail.com>
2022-12-01 15:15:16 +0100

    Updated meta-schema test to make clear there is a bug and tagged it as @optional

--- tests/codeceptjs/meta-schema_test.js
+++ tests/codeceptjs/meta-schema_test.js
@@ -4,15 +4,16 @@ const { DEFAULT_WAIT_TIME } = require('./test-config')
 Feature('core')
 
 // https://github.com/json-editor/json-editor/issues/823
-Scenario('work with json-schema meta-schema', async (I) => {
+Scenario('work with json-schema meta-schema @meta-schema @optional', async (I) => {
   I.amOnPage('issues/issue-gh-823-meta-schema.html')
   I.waitForElement('[data-schemapath="root"] h3', DEFAULT_WAIT_TIME)
   I.click('Object Properties')
   I.click('options')
   I.see('$ref')
   I.see('options')
-  // pause();
   I.click('Object Properties')
   I.click('Object Properties')
   I.see('options')
+  I.dontSee('Value must validate against exactly one of the provided schemas. It currently validates against 0 of the schemas.')
+  I.dontSee('No additional properties allowed, but property title is set')
 })
