ae9edcebb645a734ba42418b55b0ad631bcd2910
German Bisurgi <gebisurgi@protonmail.com>
2023-01-18 15:36:46 +0100

    Fix for #1164: Null values included in enums for no reason. Also fixed bug for radio editors not recognising numerical values.

--- src/editors/radio.js
+++ src/editors/radio.js
@@ -21,12 +21,6 @@ export class RadioEditor extends SelectEditor {
       this.onChange(true)
     }
 
-    if (!this.isRequired()) {
-      this.enum_display.shift()
-      this.enum_options.shift()
-      this.enum_values.shift()
-    }
-
     for (let i = 0; i < this.enum_values.length; i++) {
       /* form radio elements */
       const attributes = {
@@ -109,6 +103,10 @@ export class RadioEditor extends SelectEditor {
   }
 
   setValue (val) {
+    if (typeof val !== 'string') {
+      val = String(val)
+    }
+
     for (let i = 0; i < this.radioGroup.length; i++) {
       if (this.radioGroup[i].value === val) {
         this.radioGroup[i].checked = true
