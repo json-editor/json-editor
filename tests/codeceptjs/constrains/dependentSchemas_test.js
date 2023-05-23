77641a8234b34ac8b69384c6bb0bd97672d2ef69
German Bisurgi <gebisurgi@protonmail.com>
2023-05-23 16:35:46 +0200

    dependentSchemas support + tests

---
+++ tests/codeceptjs/constrains/dependentSchemas_test.js
@@ -0,0 +1,15 @@
+/* global Feature Scenario */
+
+Feature('dependentSchemas')
+
+Scenario('@dependentSchemas should display validation errors', ({ I }) => {
+  I.amOnPage('dependentSchemas.html')
+  I.waitForElement('.je-ready')
+  I.dontSee('Object is missing the required property \'billing_address\'')
+  I.click('[data-schemapath="root.credit_card"] .json-editor-opt-in')
+  I.waitForText('Object is missing the required property \'billing_address\'')
+  I.click('[data-schemapath="root.billing_address"] .json-editor-opt-in')
+  I.dontSee('Object is missing the required property \'billing_address\'')
+  I.click('[data-schemapath="root.credit_card"] .json-editor-opt-in')
+  I.dontSee('Object is missing the required property \'billing_address\'')
+})
