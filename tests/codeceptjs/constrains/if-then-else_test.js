02e7fe1dcce2cedb47bb9e71ad8bb8f150e8cb90
German Bisurgi <g.bisurgi@herzogkommunikation.de>
2023-06-28 10:02:47 +0200

    Added tests for multiple editor and keep_only_existing_values option

--- tests/codeceptjs/constrains/if-then-else_test.js
+++ tests/codeceptjs/constrains/if-then-else_test.js
@@ -141,3 +141,30 @@ Scenario('validate agaist if-else @if-then-else', async ({ I }) => {
   I.pressKey('Tab')
   I.dontSee('.invalid-feedback')
 })
+
+Scenario('validate agaist if-then-else when disabled fields are present in fields @if-then-else', async ({ I }) => {
+  I.amOnPage('if-then-else-disable-fields.html')
+  I.waitForElement('.je-ready')
+
+  I.selectOption('[name="root[country]"]', 'America')
+  I.fillField('[name="root[postal_code]"]', 'K1M 1M4')
+  I.pressKey('Tab')
+  I.waitForElement('.invalid-feedback')
+  I.waitForText('Value must match the pattern [0-9]{5}(-[0-9]{4})?.')
+
+  I.selectOption('[name="root[country]"]', 'Canada')
+  I.fillField('[name="root[postal_code]"]', '10000')
+  I.pressKey('Tab')
+  I.waitForElement('.invalid-feedback')
+  I.waitForText('Value must match the pattern [A-Z][0-9][A-Z] [0-9][A-Z][0-9].')
+
+  I.selectOption('[name="root[country]"]', 'America')
+  I.fillField('[name="root[postal_code]"]', '10000')
+  I.pressKey('Tab')
+  I.dontSee('.invalid-feedback')
+
+  I.selectOption('[name="root[country]"]', 'Canada')
+  I.fillField('[name="root[postal_code]"]', 'K1M 1M4')
+  I.pressKey('Tab')
+  I.dontSee('.invalid-feedback')
+})
