40c192eac2eb5ee85aa746662edf7b5ec47ee66b
German Bisurgi <g.bisurgi@herzogkommunikation.de>
2024-04-23 15:39:28 +0200

    remove not needed screenshots that produce not needed artifacts

--- tests/codeceptjs/editors/integer_test.js
+++ tests/codeceptjs/editors/integer_test.js
@@ -71,13 +71,9 @@ Scenario('should be readonly if specified and not disabled @readOnly', async ({ 
 
 Scenario('should update output when (method) setValue is called', async ({ I }) => {
   I.amOnPage('integer.html')
-  I.saveScreenshot('integer-setvalue-1.png')
   I.waitForText('5', DEFAULT_WAIT_TIME, '[data-schemapath="root.integer_range"] output')
-  I.saveScreenshot('integer-setvalue-2.png')
   I.click('.set-value')
-  I.saveScreenshot('integer-setvalue-3.png')
   I.waitForText('2', DEFAULT_WAIT_TIME, '[data-schemapath="root.integer_range"] output')
-  I.saveScreenshot('integer-setvalue-4.png')
 })
 
 Scenario('should validate value', async ({ I }) => {
