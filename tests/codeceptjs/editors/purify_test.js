d14b307d434090a8b2489d336b311953a93b0cff
German Bisurgi <g.bisurgi@herzogkommunikation.de>
2026-02-05 14:58:09 +0100

    Improved test selector

--- tests/codeceptjs/editors/purify_test.js
+++ tests/codeceptjs/editors/purify_test.js
@@ -4,15 +4,18 @@ Feature('purify')
 
 Scenario('Should @purify XSS from direct input', async ({ I }) => {
   I.amOnPage('purify.html')
+  I.waitForElement('[name="root[string]"]')
   I.fillField('[name="root[string]"]', 'bla</script><script>alert(1)</script>')
   I.click('.get-value')
-  I.waitForValue('.value', '{"string":"bla"}')
+  I.waitForValue('#value', '{"string":"bla"}')
 })
 
 Scenario('Should @purify XSS from setValue() via textarea', async ({ I }) => {
   I.amOnPage('purify.html')
+  I.waitForElement('#value')
   I.fillField('#value', 'bla</script><script>alert(1)</script>')
   I.click('.set-value')
+  I.wait(1)
   I.click('.get-value')
-  I.waitForValue('.value', '{"string":"bla"}')
+  I.waitForValue('#value', '{"string":"bla"}')
 })
